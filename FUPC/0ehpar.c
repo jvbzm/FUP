058bdc5dfc5496cb1cd9c84d16b192f7c4420a4f
jvbzm <victormb1009@gmail.com>
2025-05-14 15:06:39 -0300

    sim

---
+++ FUPC/0ehpar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int n1,n2,par;
+    par=0;
+    scanf("%d%d",&n1,&n2);
+    if(n2<n1){
+        printf("invalido");
+        return 0;
+    }
+    if(n1 % 2 !=0){
+        n1++;
+    }
+    for(int i=n1;i<=n2;i+=2){
+        par+=i;
+    }
+    printf("%d",par);
+}
